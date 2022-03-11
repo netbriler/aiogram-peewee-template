7083657d97bc29fe054a601dcf3fadb312427acf
netbriler <netbriler@gmail.com>
2022-03-11 17:42:58 +0200

    Support MemoryStorage or RedisStorage choice

--- loader.py
+++ loader.py
@@ -5,7 +5,14 @@ from data import config
 from middlewares.i18n import i18n
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2(config.REDIS_HOST, config.REDIS_PORT, db=6)
+
+if config.REDIS_HOST and config.REDIS_PORT:
+    from aiogram.contrib.fsm_storage.redis import RedisStorage2
+    storage = RedisStorage2(config.REDIS_HOST, config.REDIS_PORT, db=6)
+else:
+    from aiogram.contrib.fsm_storage.memory import MemoryStorage
+    storage = MemoryStorage()
+
 dp = Dispatcher(bot, storage=storage, run_tasks_by_default=True)
 
 _ = i18n.gettext
