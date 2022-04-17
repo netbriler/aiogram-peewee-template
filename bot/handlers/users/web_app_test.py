aef33bb791e1ba28f612b0a797dc960b6992b5e8
netbriler <netbriler@gmail.com>
2022-04-17 10:50:57 +0300

    Change web app preview link

--- bot/handlers/users/web_app_test.py
+++ bot/handlers/users/web_app_test.py
@@ -2,18 +2,17 @@ import time
 
 from aiogram.types import Message, ContentTypes, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-from loader import dp
+from loader import dp, bot
 from models import User
 
 
-@dp.message_handler(commands=['web_app_test'])
+@dp.message_handler(commands=['test'])
 async def _web_app_init(message: Message, user: User):
-    web_app_uri = 'https://htmlpreview.github.io/?https://raw.githubusercontent.com/' \
-                  'netbriler/aiogram-sqlalchemy-template/master/web_app_echo.html?time=' + str(time.time())
+    web_app_uri = 'https://aiogram-sqlalchemy-template.vercel.app/web_app_echo.html?time=' + str(time.time())
+
+    print(web_app_uri)
 
     web_app = WebAppInfo(url=web_app_uri)
-
-    await bot.set_chat_menu_button(message.chat.id, MenuButtonWebApp(text='Menu', web_app=web_app))
 
     markup = InlineKeyboardMarkup()
     markup.add(
@@ -21,8 +20,8 @@ async def _web_app_init(message: Message, user: User):
     )
 
     await message.answer(web_app_uri)
-
     await message.answer('Web app test', reply_markup=markup)
+    await bot.set_chat_menu_button(message.chat.id, MenuButtonWebApp(text='Menu', web_app=web_app))
 
 
 @dp.message_handler(content_types=[ContentTypes.WEB_APP_DATA])
