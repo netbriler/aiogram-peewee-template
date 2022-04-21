3ea6d8ce042e69a180cee4f914d24ba57358ee17
netbriler <netbriler@gmail.com>
2022-04-21 22:17:25 +0300

    Web app change button type

--- bot/handlers/users/web_app_test.py
+++ bot/handlers/users/web_app_test.py
@@ -1,8 +1,6 @@
-import json
 import time
 
-from aiogram.types import Message, ContentTypes, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, \
-    MenuButtonWebApp
+from aiogram.types import Message, ContentTypes, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, MenuButtonWebApp
 
 from loader import dp, bot
 from models import User
@@ -12,23 +10,18 @@ from models import User
 async def _web_app_init(message: Message, user: User):
     web_app_uri = 'https://aiogram-sqlalchemy-template.vercel.app/web_app_echo.html?time=' + str(time.time())
 
-    web_app = WebAppInfo(url=web_app_uri)
-
-    markup = InlineKeyboardMarkup()
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(
-        InlineKeyboardButton('Menu', web_app=web_app)
+        KeyboardButton('Menu', web_app=WebAppInfo(url=web_app_uri + '&keyboard_button=1'))
     )
 
-    await message.answer(web_app_uri)
     await message.answer('Web app test', reply_markup=markup)
-    await bot.set_chat_menu_button(message.chat.id, MenuButtonWebApp(text='Menu', web_app=web_app))
+    await bot.set_chat_menu_button(message.chat.id, MenuButtonWebApp(text='Menu', web_app=WebAppInfo(url=web_app_uri)))
 
 
 @dp.message_handler(content_types=[ContentTypes.WEB_APP_DATA])
 async def _web_app(message: Message, user: User):
-    data = json.loads(message.web_app_data.data)['data']
-
     text = f'Button text: {message.web_app_data.button_text}\n\n' \
-           f'Data:\n<pre>{data}</pre>'
+           f'Data:\n<pre>{message.web_app_data.data}</pre>'
 
     await message.answer(text)
